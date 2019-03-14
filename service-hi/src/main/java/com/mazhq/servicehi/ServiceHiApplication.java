ea6fee093ca459df00d574bbb03379a848091a62
马振全 <mazhq@youyuan.com>
2019-03-14 18:12:38 +0800

    提交最新代码

--- service-hi/src/main/java/com/mazhq/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/mazhq/servicehi/ServiceHiApplication.java
@@ -1,13 +1,26 @@
 package com.mazhq.servicehi;
 
+import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-/** 
+import org.springframework.web.client.RestTemplate;
+
+/**
 * @Description: TODO
 * @param null
 * @return 
@@ -17,21 +30,61 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
 public class ServiceHiApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
+    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
 
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+    @RequestMapping("/hiMiya")
+    public String hiMiya(){
+        logger.info("hi is being called");
+        return "hi i'm miya!";
+    }
+
+    @RequestMapping("/miya")
+    public String miya(){
+        logger.info("info is being called");
+        return restTemplate.getForObject("http://localhost:8762/info",String.class);
+    }
+
+
+    @RequestMapping("/info")
+    public String info(){
+        logger.info("hi is being called");
+        return "i'm service-hi";
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
 
 
